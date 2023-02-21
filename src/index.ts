d71b9666750497e4ce3cf97b58dc2c565e4b7650
Dau Khoa <khoakomlem@gmail.com>
2023-02-21 14:18:38 +0700

    Add terminal title in4, yarnlock @colyseus/montior

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import {Server} from 'colyseus';
 import {WebSocketTransport} from '@colyseus/ws-transport';
 import {createServer} from 'http';
 import express from 'express';
+import {monitor} from '@colyseus/monitor';
 import CasualRoom from './room/Casual.js';
+
+const setTerminalTitle = (text: string) => {
+	process.stdout.write(
+		`${String.fromCharCode(27)}]0;${text}${String.fromCharCode(7)}`,
+	);
+};
 
 const port = Number(process.env.port) || 3000;
 const app = express();
 app.use(express.json());
+app.use('/colyseus', monitor());
 
 const gameServer = new Server({
 	transport: new WebSocketTransport({
@@ -16,10 +24,16 @@ const gameServer = new Server({
 	}),
 });
 
-try {
-	await gameServer.listen(port);
-	console.log(`Server listening on port ${port}`);
-	gameServer.define('casual', CasualRoom).enableRealtimeListing();
-} catch (err) {
-	console.error(err);
-}
+setInterval(() => {
+	const memoryUsage = process.memoryUsage().heapTotal / 1024 / 1024;
+	const symbol = '=';
+	setTerminalTitle(
+		`GUNSURVIVAL 3 SERVER - MEMORY: ${memoryUsage.toFixed(
+			2,
+		)}MB`,
+	);
+}, 1000);
+
+gameServer.listen(port).catch(console.error);
+console.log(`Server listening on port ${port}`);
+gameServer.define('casual', CasualRoom).enableRealtimeListing();
