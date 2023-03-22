97d523d0827c728bf01c42dddafb03396b965543
khoakomlem <khoakomlem@gmail.com>
2023-03-22 23:32:13 +0700

    Bullet state optimize

--- src/entity/Bullet.ts
+++ src/entity/Bullet.ts
@@ -1,7 +1,7 @@
 import {type, Schema} from '@colyseus/schema';
 import {getStats} from '@gunsurvival/core';
 import type * as EntityCore from '@gunsurvival/core/entity';
-import Entity, {VectorSchema} from './Entity.js';
+import Entity from './Entity.js';
 
 export class StatsBullet extends Schema {
 	@type('number') radius: number;
@@ -9,11 +9,18 @@ export class StatsBullet extends Schema {
 
 export default class Bullet extends Entity {
 	@type(StatsBullet) stats = new StatsBullet().assign(getStats('Bullet'));
+	@type('number') speed = 0;
 
 	declare entityCore: EntityCore.Bullet;
 
 	update() {
-		super.update();
+		// This.angle = this.entityCore.body.angle;
+		// this.scale = this.entityCore.body.scale;
+		// This.speed = this.entityCore.speed;
+		// super.update();
+		this.updateStats(this.entityCore.stats, this.stats);
+	}
+
 	init(entityCore: EntityCore.Bullet) {
 		super.init(entityCore);
 		this.speed = entityCore.speed;
